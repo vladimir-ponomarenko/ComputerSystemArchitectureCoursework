0902a063071baeb96346d6d8b59886a519e9e4f7
User <vova102204@gmail.com>
2024-11-23 12:10:00 +0700

    АВС - ргр

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
     "fmt"
+    "log"
     "net/http"
+    "github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello, World!")
-}
-
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    r := mux.NewRouter()
+    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintln(w, "Hello, World!")
+    })
+
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
